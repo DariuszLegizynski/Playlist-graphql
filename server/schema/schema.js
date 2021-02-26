2f59b1460fb3901f8b7f600ba801573044716a73
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-02-26 13:57:07 +0100

    how to make type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,22 +5,61 @@ const _ = require("lodash");
 const {
 	GraphQLObjectType,
 	GraphQLString,
+	GraphQLID,
+	GraphQLInt,
 	GraphQLSchema,
 } = graphql;
 
 // mockup db
 const books = [
-	{ name: "first book", genre: "first genre", id: "1" },
-	{ name: "second book", genre: "second genre", id: "2" },
-	{ name: "third book", genre: "third genre", id: "3" },
+	{
+		name: "first book",
+		genre: "first genre",
+		id: "1",
+		authorId: "3",
+	},
+	{
+		name: "second book",
+		genre: "second genre",
+		id: "2",
+		authorId: "1",
+	},
+	{
+		name: "third book",
+		genre: "third genre",
+		id: "3",
+		authorId: "2",
+	},
+];
+
+const authors = [
+	{ name: "first name", age: 34, id: "1" },
+	{ name: "second eman", age: 43, id: "2" },
+	{ name: "third time", age: 54, id: "3" },
 ];
 
 const BookType = new GraphQLObjectType({
 	name: "Book",
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString },
+		author: {
+			type: AuthorType,
+			resolve(parent, args) {
+				console.log(parent);
+				return _.find(authors, { id: parent.authorId });
+			},
+		},
+	}),
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: "Author",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
 	}),
 });
 
@@ -29,10 +68,17 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		book: {
 			type: BookType,
-			args: { id: { type: GraphQLString } },
+			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				// code to get data from db / other source
 				return _.find(books, { id: args.id });
+			},
+		},
+		author: {
+			type: AuthorType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return _.find(authors, { id: args.id });
 			},
 		},
 	},
